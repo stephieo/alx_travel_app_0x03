6a2289d60f64af617445a36c94a97b049f56ea85
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-06-01 19:39:21 +0100

    remove validators

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -8,25 +8,25 @@ class ListingSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['listing_id', 'user_id','created_at']
     
-    def validate_title(self, value):
-        if not value.strip():
-            raise serializers.ValidationError("Title cannot be empty")
+    # def validate_title(self, value):
+    #     if not value.strip():
+    #         raise serializers.ValidationError("Title cannot be empty")
 
-    def validate_description(self, value):
-        if not value.strip():
-            raise serializers.ValidationError("Description cannot be empty")
+    # def validate_description(self, value):
+    #     if not value.strip():
+    #         raise serializers.ValidationError("Description cannot be empty")
 
-    def validate_destination(self, value):
-        if not value.strip():
-            raise serializers.ValidationError("Destination cannot be empty")
+    # def validate_destination(self, value):
+    #     if not value.strip():
+    #         raise serializers.ValidationError("Destination cannot be empty")
 
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError("price  must be greater than 0")
+    # def validate_price(self, value):
+    #     if value < 0:
+    #         raise serializers.ValidationError("price  must be greater than 0")
 
-    def validate(self, data):
-        if data['end_date'] < data['start_date']:
-            raise serializers.ValidationError("Start date must occur before end date")
+    # def validate(self, data):
+    #     if data['end_date'] < data['start_date']:
+    #         raise serializers.ValidationError("Start date must occur before end date")
 
 
 class BookingSerializer(serializers.ModelSerializer):
@@ -35,7 +35,7 @@ class BookingSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = [ 'user_id', 'listing_id', 'booked_at']
 
-    def validate_totalprice(self, value):
-        if value < 0:
-            raise serializers.ValidationError("price  must be greater than 0")
+    # def validate_totalprice(self, value):
+        # if value < 0:
+            # raise serializers.ValidationError("price  must be greater than 0")
 
