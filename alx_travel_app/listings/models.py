6a025b0d402e003f7f22cb61ebc5b4754f4ca327
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-06-02 00:04:02 +0100

    feat: add validators to Listing and BOoking

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -23,7 +23,7 @@ class User(models.Model):
 class Listing(models.Model):
     listing_id = models.UUIDField(primary_key=True,
                                   default=uuid.uuid4, editable=False)
-    host_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    host_id = models.ForeignKey('user_id', on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     description = models.TextField()
     location = models.CharField(max_length=250)
--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -6,7 +6,7 @@ class ListingSerializer(serializers.ModelSerializer):
         ''' this  identifies which model the serializer is tied to and the fields  to (de)serialize'''
         model = Listing
         fields = '__all__'
-        read_only_fields = ['listing_id', 'user_id','created_at']
+        read_only_fields = ['listing_id', 'host_id','created_at', 'updated_at']
     
     def validate_title(self, value):
         if not value.strip():
@@ -16,9 +16,17 @@ class ListingSerializer(serializers.ModelSerializer):
         if not value.strip():
             raise serializers.ValidationError("Description cannot be empty")
 
+    def validate_location(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("location cannot be empty")
+
     def validate_price_per_night(self, value):
         if value < 0:
             raise serializers.ValidationError("price  must be greater than 0")
+    
+    def validate_max_guests(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Max guests must be greater than 0")
 
 
 
@@ -28,11 +36,15 @@ class BookingSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = [ 'user_id', 'listing_id', 'booked_at']
 
-    def validate_totalprice(self, value):
+    def validate_total_price(self, value):
         if value < 0:
             raise serializers.ValidationError("price  must be greater than 0")
 
     def validate(self, data):
         if data['end_date'] < data['start_date']:
             raise serializers.ValidationError("Start date must occur before end date")
+        if data['start_date'] < now():
+            raise serializers.ValidationError("Start date must be in the future")
+    
+    #TODO: add validation for overlapping bookings and one to check for valid status
 
